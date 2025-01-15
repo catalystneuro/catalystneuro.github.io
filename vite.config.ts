c2152beabb1ce6fe0b23ac9580f4d26ec6cb8948
Ben Dichter <ben.dichter@gmail.com>
2025-01-15 15:48:34 -0600

    Update base URL for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import mdx from "@mdx-js/rollup";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/catalystneuro.github.io/',
+  base: '/',
   server: {
     host: "::",
     port: 8080,
