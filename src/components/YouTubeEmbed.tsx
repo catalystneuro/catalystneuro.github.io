635491c9972fe300cc6b0293e30a37d4f921683d
Ben Dichter <ben.dichter@gmail.com>
2025-01-18 14:52:10 -0600

    feat: update YouTubeEmbed component to use aspect ratio for responsive design

--- src/components/YouTubeEmbed.tsx
+++ src/components/YouTubeEmbed.tsx
@@ -4,7 +4,7 @@ interface YouTubeEmbedProps {
 
 export const YouTubeEmbed = ({ videoId }: YouTubeEmbedProps) => {
   return (
-    <div className="w-full max-w-4xl mx-auto rounded-lg overflow-hidden shadow-lg" style={{ height: '500px' }}>
+    <div className="relative w-full max-w-4xl mx-auto rounded-lg overflow-hidden shadow-lg aspect-video">
       <iframe
         src={`https://www.youtube.com/embed/${videoId}`}
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
